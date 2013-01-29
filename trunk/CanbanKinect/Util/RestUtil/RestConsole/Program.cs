f509ea98cda3d2baf1c2b3313505ae68a807f9f3
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2013-01-29 15:57:55 +0000

    rest läuft

---
+++ trunk/CanbanKinect/Util/RestUtil/RestConsole/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using De.Cefoot.RestUtil;
+using De.Cefoot.RestUtil.RestWorker;
+
+namespace RestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            Console.Write("Port:");
+            var port = 0;
+            try
+            {
+                var tmp = Console.ReadLine();
+                port = int.Parse(tmp);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Start();
+                return;
+            }
+            Console.Write("Path?:");
+            String preString = "";
+            try
+            {
+                var tmp = Console.ReadLine();
+                var builder = new UriBuilder("http","google",port);
+                if (tmp != null) builder.Path = tmp;
+                var uri = builder.Uri;
+                preString = tmp;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Start();
+                return;
+            }
+            RestFactory restFactory = null;
+            try
+            {
+                restFactory = De.Cefoot.RestUtil.RestFactory.CreateRestWorker(port, preString, new UrlMethodCallWorker());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.Write("elevate?(y/n):");
+                if(Console.ReadKey().KeyChar.ToString().ToLower() == "y")
+                {
+                    Elevate();
+                }
+                return;
+            }
+            restFactory.Start();
+            while (Console.ReadLine() != "quit")
+            {
+                Console.WriteLine("\"quit\" to exit");
+            }
+        }
+
+        private static void Elevate()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var procInfo = new ProcessStartInfo(executingAssembly.Location)
+                               {
+                                   UseShellExecute = true,
+                                   Verb = "runas",
+                                   WindowStyle = ProcessWindowStyle.Normal
+                               };
+
+            Process.Start(procInfo);
+            Process.GetCurrentProcess().Close();
+        }
+    }
+}
---
+++ trunk/CanbanKinect/Util/RestUtil/RestUtil/RestFactory.cs
@@ -0,0 +1,179 @@
+﻿/* 
+  ------------------------------------------------------------------------------
+        (c) cefoot
+
+  Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+  dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+  Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+  noch manipuliert werden.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Xml;
+using System.Linq;
+using System.Linq.Expressions;
+using De.Cefoot.RestUtil.RestWorker;
+using HttpNamespaceManager.Lib;
+using HttpNamespaceManager.Lib.AccessControl;
+using log4net;
+
+namespace De.Cefoot.RestUtil
+{
+
+    public class RestFactory
+    {
+
+        #region Variablen
+
+        #endregion
+
+        #region Konstruktoren
+
+        private RestFactory()
+        {
+
+        }
+
+        #endregion
+
+        #region Eigenschaften
+
+        private static ILog Logger
+        {
+            get
+            {
+                return LogManager.GetLogger("RestFactory");
+            }
+        }
+        public IRestWorker GetWorker { get; set; }
+        public IRestWorker PutWorker { get; set; }
+        public IRestWorker PostWorker { get; set; }
+        public IRestWorker DeleteWorker { get; set; }
+        private HttpApi _httpApi;
+
+        public HttpApi HttpApi
+        {
+            get
+            {
+                return _httpApi ?? (_httpApi = new HttpApi());
+            }
+        }
+
+        protected HttpListener RestListerner { get; set; }
+
+        #endregion
+
+        #region Methoden
+        [DllImport("shell32.dll")]
+        internal static extern bool IsUserAnAdmin();
+
+        public static RestFactory CreateRestWorker(int port, string urlPreString, IRestWorker getWorker, IRestWorker putWorker = null, IRestWorker postWorker = null, IRestWorker deleteWorker = null)
+        {
+            if(!IsUserAnAdmin())
+            {
+                throw new UnauthorizedAccessException("Muss als Admin ausgeführt werden");
+            }
+            var factory = new RestFactory
+                              {
+                                  GetWorker = getWorker ?? new NotFoundWorker(),
+                                  PutWorker = putWorker ?? new NotFoundWorker(),
+                                  PostWorker = postWorker ?? new NotFoundWorker(),
+                                  DeleteWorker = deleteWorker ?? new NotFoundWorker(),
+                                  RestListerner = new HttpListener()
+                              };
+            factory.AddPrefix(port, urlPreString);
+            return factory;
+        }
+
+        ~RestFactory()
+        {
+            HttpApi.Dispose();
+        }
+
+        private void AddPrefix(int port, string urlPreString)
+        {
+            var uriPrefix = String.Format("http://+:{0}/{1}", port, urlPreString);
+            Logger.InfoFormat("Listening on:{0}", uriPrefix);
+            AddRights(uriPrefix);
+            RestListerner.Prefixes.Add(uriPrefix);
+        }
+
+        private void AddRights(string uriPrefix)
+        {
+            if(HttpApi.QueryHttpNamespaceAcls().ContainsKey(uriPrefix))
+            {
+                HttpApi.RemoveHttpHamespaceAcl(uriPrefix);
+            }
+            var newSd = new SecurityDescriptor
+                            {
+                                DACL = new AccessControlList()
+                            };
+            var sId = SecurityIdentity.SecurityIdentityFromName(WindowsIdentity.GetCurrent().Name);
+            var ace = new AccessControlEntry(sId)
+                          {
+                              AceType = AceType.AccessAllowed
+                          };
+            ace.Add(AceRights.GenericAll);
+            ace.Add(AceRights.GenericExecute);
+            ace.Add(AceRights.GenericRead);
+            ace.Add(AceRights.GenericWrite);
+            newSd.DACL.Add(ace);
+
+            HttpApi.SetHttpNamespaceAcl(uriPrefix, newSd);
+            return;
+        }
+
+        private void UrlRequest(IAsyncResult ar)
+        {
+            var httpListenerContext = RestListerner.EndGetContext(ar);
+            RestListerner.BeginGetContext(UrlRequest, null);
+            ProcessRequest(httpListenerContext);
+        }
+
+        private void ProcessRequest(HttpListenerContext httpListenerContext)
+        {
+            try
+            {
+
+                switch (httpListenerContext.Request.HttpMethod)
+                {
+                    case "GET":
+                        GetWorker.WorkOnUrl(httpListenerContext.Request, httpListenerContext.Response);
+                        break;
+                    case "PUT":
+                        PutWorker.WorkOnUrl(httpListenerContext.Request, httpListenerContext.Response);
+                        break;
+                    case "POST":
+                        PostWorker.WorkOnUrl(httpListenerContext.Request, httpListenerContext.Response);
+                        break;
+                    case "DELETE":
+                        DeleteWorker.WorkOnUrl(httpListenerContext.Request, httpListenerContext.Response);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                httpListenerContext.Response.StatusCode = 500;
+                httpListenerContext.Response.Close(Encoding.UTF8.GetBytes(ex.ToString()), false);
+            }
+        }
+
+        public void Start()
+        {
+            RestListerner.Start();
+            RestListerner.BeginGetContext(UrlRequest, null);
+        }
+
+        public void Stop()
+        {
+            RestListerner.Stop();
+        }
+
+        #endregion
+    }
+}
