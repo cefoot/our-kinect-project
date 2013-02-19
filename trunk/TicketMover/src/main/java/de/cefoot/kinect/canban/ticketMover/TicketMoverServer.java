95b009a243fbe2c2437734b4a2e9c89997ec0390
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2013-02-19 20:48:55 +0000

    pimped again

--- trunk/TicketMover/src/main/java/de/cefoot/kinect/canban/ticketMover/TicketMoverServer.java
+++ trunk/TicketMover/src/main/java/de/cefoot/kinect/canban/ticketMover/TicketMoverServer.java
@@ -1,5 +1,9 @@
 package de.cefoot.kinect.canban.ticketMover;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.inject.Inject;
 
 import org.cometd.annotation.Listener;
@@ -14,37 +18,55 @@ import org.cometd.bayeux.server.ServerSession;
 
 @Service
 public class TicketMoverServer {
-    @Inject
-    private BayeuxServer bayeuxServer;
-    @Session
-    private LocalSession sender;
-	
-    @Listener("/service/hello")
-    public void processClientHello(ServerSession session, ServerMessage message)
-    {
-        System.out.printf("Received greeting '%s' from remote client %s%n", message.getData(), session.getId());
-        sendBack();
-    }
+	@Inject
+	private BayeuxServer bayeuxServer;
+	@Session
+	private LocalSession sender;
 
-	private void sendBack() {
-		System.out.println("send");
-        // Create the channel name using the stock symbol
-        String channelName = "/hello/world";
+	@Listener("/service/hello")
+	public void processClientHello(ServerSession session, ServerMessage message) {
+		sendTickets();
+	}
 
-        // Initialize the channel, making it persistent and lazy
-        bayeuxServer.createIfAbsent(channelName, new ConfigurableServerChannel.Initializer()
-        {
-            public void configureChannel(ConfigurableServerChannel channel)
-            {
-                channel.setPersistent(true);
-                channel.setLazy(true);
-            }
-        });
+	Map<String, Map<String, Object>> tickets = new HashMap<String, Map<String, Object>>();
 
-        // Publish to all subscribers
-        ServerChannel channel = bayeuxServer.getChannel(channelName);
-        channel.publish(sender, "Hallo Zurück", null);
-        System.out.println("aaaand gone");
+	@Listener("/ticket/move")
+	public void procesClientTicketMove(ServerSession session, ServerMessage message) {
+		Map<String, Object> dataMap = message.getDataAsMap();
+		String id = dataMap.get("id").toString();
+		if (!tickets.containsKey(id)) {
+			return;
+		}
+		((Map<String,Object>)tickets.get(id).get("style")).put("left", dataMap.get("x"));
+		((Map<String,Object>)tickets.get(id).get("style")).put("top", dataMap.get("y"));
+	}
+
+	@Listener("/ticket/add")
+	public void procesClientTicketAdd(ServerSession session, ServerMessage message) {
+		Map<String, Object> dataMap = message.getDataAsMap();
+		tickets.put(dataMap.get("id").toString(), dataMap);
+	}
+
+	private void sendTickets() {
+		// Create the channel name using the stock symbol
+		String channelName = "/ticket/add";
+
+		// Initialize the channel, making it persistent and lazy
+		bayeuxServer.createIfAbsent(channelName,
+				new ConfigurableServerChannel.Initializer() {
+					public void configureChannel(
+							ConfigurableServerChannel channel) {
+						channel.setPersistent(true);
+						channel.setLazy(true);
+					}
+				});
+
+		// Publish to all subscribers
+		ServerChannel channel = bayeuxServer.getChannel(channelName);
+		Map<String, Map<String, Object>> curTicks = tickets;
+		for (Entry<String, Map<String, Object>> ticket : curTicks.entrySet()) {
+			channel.publish(sender, ticket.getValue(), null);
+		}
 	}
 
 }
