8792e104ca446d7725c4ebce34567d61e7058b4a
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2013-05-29 13:54:42 +0000

    kinect video

---
+++ trunk/Cam/Cam/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace Cam
+{
+    public partial class Form1 : Form
+    {
+        private KinectSensor _kinectSensor;
+        private byte[] pixels;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _kinectSensor = KinectSensor.KinectSensors[0];
+            pixels = new byte[_kinectSensor.ColorStream.FramePixelDataLength];
+
+            wBitmap = new WriteableBitmap(sensor.ColorStream.FrameWidth, sensor.ColorStream.FrameHeight,
+                96, 96, PixelFormats.Bgra32, null);
+            _kinectSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            _kinectSensor.ColorFrameReady += _kinectSensor_ColorFrameReady;
+        }
+
+        void _kinectSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/Cam/CamWPF/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Coding4Fun.Kinect.Wpf;
+using Microsoft.Kinect;
+
+namespace CamWPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private KinectSensor nui;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetupKinect();
+            nui.Start();
+        }
+        private void SetupKinect()
+        {
+            if (KinectSensor.KinectSensors.Count== 0)
+            {
+                this.Title = "No Kinect connected";
+            }
+            else
+            {
+                //use first Kinect
+                nui = KinectSensor.KinectSensors[0];
+
+                //Initialize to return both Color & Depth images
+                nui.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);// .Initialize(RuntimeOptions.UseColor | RuntimeOptions.UseDepth));
+                nui.ColorFrameReady += NuiColorFrameReady;
+            }
+        }
+
+        void NuiColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var image = e.OpenColorImageFrame())
+            {
+                if(image == null) return;
+                var data = new byte[image.PixelDataLength];
+                image.CopyPixelDataTo(data);
+                image1.Source = data.ToBitmapSource(image.Width, image.Height);
+            }
+        }
+    }
+}
