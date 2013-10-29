b354f7c307fa4b5e6fbe503a7ba583c46327c203
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2013-10-29 15:50:00 +0000

    dummy websocket .net client

---
+++ trunk/Websocket/Net/SocketListener/SocketListener/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading;
+using SimpleJson;
+using WebSocket4Net;
+
+namespace SocketListener
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var socket = new CometdSocket("ws://localhost:8080/socketBtn");
+            socket.Subscribe("/paint/", HandleTextRecieved);
+
+            Console.WriteLine(socket.Handshaked);
+            Console.ReadKey();
+        }
+
+        private static void HandleTextRecieved(JsonObject msg)
+        {
+            //data enthält den text, wenn kein objekt übergeben wirdansonsten ist in data eine hashmap
+            Console.WriteLine(msg["data"].ToString());
+        }
+    }
+}
