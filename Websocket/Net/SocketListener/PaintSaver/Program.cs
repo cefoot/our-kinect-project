241ce9445a6731ae51e5261d4f7750fec9294bbe
bastiankayser84 <bastiankayser84@7954b5d7-b0db-85ae-a50a-688a78da6528>
2013-11-05 15:01:46 +0000

    Kinect mit ins projekt aufgenommen

--- Websocket/Net/SocketListener/PaintSaver/Program.cs
+++ Websocket/Net/SocketListener/PaintSaver/Program.cs
@@ -4,19 +4,64 @@ using System.Linq;
 using System.Text;
 using SimpleJson;
 using SocketListener;
+using Microsoft.Kinect;
 
 namespace PaintSaver
 {
     class Program
     {
+        KinectSensor kinect;
+        Boolean isRecording = false;
         static void Main(string[] args)
+        {
+            Program pro = new Program();
+            pro.start();
+            Console.ReadLine();
+          
+        }
+
+        private void start()
         {
             CometdSocket socket = new CometdSocket("ws://localhost:8080/socketBtn");
             socket.Subscribe("/paint/", PaintHandler);
         }
 
-        private static void PaintHandler(JsonObject msg)
+        private void initKinectSensor()
         {
+            this.kinect = KinectSensor.KinectSensors.Where(x => x.Status == KinectStatus.Connected).FirstOrDefault();
+            
+
+        }
+
+        private void registerEventListener()
+        {
+            this.kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(Sensor_SkeletonFrameReady);
+            this.kinect.SkeletonStream.Enable();
+            this.kinect.Start();
+        }
+
+
+        void Sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                if (frame != null)
+                {
+                    if (isRecording)
+                    {
+                        
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+        }
+
+        private void PaintHandler(JsonObject msg)
+        {
+
             switch (msg["data"].ToString())
             {
                 case "start":
@@ -28,14 +73,15 @@ namespace PaintSaver
             }
         }
 
-        private static void StopRecognize()
+        private void StopRecognize()
         {
-            throw new NotImplementedException();
+            this.isRecording = true;
         }
 
-        private static void StartRecognize()
+        private void StartRecognize()
         {
-            throw new NotImplementedException();
+
+            this.isRecording = false;
         }
     }
 }
