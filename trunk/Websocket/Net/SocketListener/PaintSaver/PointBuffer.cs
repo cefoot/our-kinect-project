616437b675b396c98f562370915347ac8af6d66c
bastiankayser84 <bastiankayser84@7954b5d7-b0db-85ae-a50a-688a78da6528>
2013-12-06 23:42:14 +0000

    Buffer eingebaut um malen und betrachten flüssiger zu machen

---
+++ trunk/Websocket/Net/SocketListener/PaintSaver/PointBuffer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace SocketListener
+{
+    class PointBuffer
+    {
+        int count = 0;
+        int minElements = 1;
+        float curX = 1;
+        float curY = 1;
+        float curZ = 1;
+        bool ready = false;
+        Queue<float> lastX;
+        Queue<float> lastY;
+        Queue<float> lastZ;
+        int currentElementPointer = 0;
+        String name = "";
+        public PointBuffer(int min,String name)
+        {
+            minElements = min;
+            lastX = new Queue<float>(minElements);
+            lastY = new Queue<float>(minElements);
+            lastZ = new Queue<float>(minElements);
+            this.name = name;
+        }
+
+        public void setMinElements(int min)
+        {
+            this.minElements = min;
+        }
+
+
+
+        public SkeletonPoint getCurrentPoint(float scale)
+        {
+
+            SkeletonPoint currentPoint = new SkeletonPoint();
+            currentPoint.X = curX * scale;
+            currentPoint.Y = curY * scale;
+            currentPoint.Z = curZ * scale;
+            return currentPoint;
+
+        }
+        public SkeletonPoint add(SkeletonPoint newPoint)
+        {
+            return this.add(newPoint, 1f);
+        }
+
+        public SkeletonPoint add(SkeletonPoint newPoint,float scale)
+        {
+            lastX.Enqueue(newPoint.X);
+            lastY.Enqueue(newPoint.Y);
+            lastZ.Enqueue(newPoint.Z);
+
+            if (!ready)
+            {
+
+                count++;
+                if (count >= minElements)
+                {
+                    ready = true;
+                    Console.WriteLine(name+" ready");
+                }
+            }
+            else
+            {
+                lastX.Dequeue();
+                lastY.Dequeue();
+                lastZ.Dequeue();
+            }
+            calcNewPoint(newPoint);
+
+            return getCurrentPoint(scale);
+        }
+
+        private void calcNewPoint(SkeletonPoint newPoint)
+        {
+
+            
+            for (int i = 0; i < lastX.Count; i++)
+            {
+                curX = lastX.Average();
+                curY = lastY.Average();
+                curZ = lastZ.Average();
+            }
+             
+
+        }
+
+        public void resetBuffer()
+        {
+            lastX.Clear();
+            lastY.Clear();
+            lastZ.Clear();
+
+            curX = 1;
+            curY = 1;
+            curZ = 1;
+
+            this.ready = false;
+
+            count = 0;
+        }
+
+        public bool isBufferReady()
+        {
+            return ready;
+        }
+
+    }
+}
