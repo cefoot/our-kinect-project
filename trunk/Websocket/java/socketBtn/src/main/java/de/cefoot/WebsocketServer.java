c87e64b0d1ce4dc439483de2aff73744b4739a9d
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2014-01-07 15:51:24 +0000

    server: farbe jetzt immer vom server drawings in set gespeichert client drawing vom server und ein wenig aufgeräumt

--- trunk/Websocket/java/socketBtn/src/main/java/de/cefoot/WebsocketServer.java
+++ trunk/Websocket/java/socketBtn/src/main/java/de/cefoot/WebsocketServer.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import javax.inject.Inject;
 
 import org.cometd.annotation.Listener;
 import org.cometd.annotation.Service;
 import org.cometd.annotation.Session;
+import org.cometd.bayeux.MarkedReference;
 import org.cometd.bayeux.server.BayeuxServer;
 import org.cometd.bayeux.server.ConfigurableServerChannel;
 import org.cometd.bayeux.server.LocalSession;
@@ -35,33 +37,21 @@ public class WebsocketServer {
 
 	@Listener("/drawing/hello")
 	public void processDrawingHello(ServerSession session, ServerMessage message) {
-		String channelName = "/datachannel/positionData";// Initialize the
-															// channel, making
-															// it persistent and
-															// lazy
-		bayeuxServer.createIfAbsent(channelName,
-				new ConfigurableServerChannel.Initializer() {
-					public void configureChannel(
-							ConfigurableServerChannel channel) {
-						channel.setPersistent(true);
-						channel.setLazy(true);
-					}
-				});
-
+		String channelName = "/datachannel/positionData";
 		// Publish to all subscribers
-		ServerChannel channel = bayeuxServer.getChannel(channelName);
+		ServerChannel channel = getChannel(channelName);
 		for (Object drawing : drawings) {
 			channel.publish(sender, drawing, null);
 		}
 	}
-	
+
 	@Listener("/datachannel/clear")
 	public void processClear(ServerSession session, ServerMessage message) {
 		drawings.clear();
-		drawIdx = 0;
+		newDrawing = true;
 	}
 
-	@Listener("/datachannel/handPosition")
+	@Listener("/datachannel/drawPosition")
 	public void processHandPos(ServerSession session, ServerMessage message) {
 		try {
 			long x = (long) message.getDataAsMap().get("X");
@@ -88,39 +78,48 @@ public class WebsocketServer {
 		// Create the channel name using the stock symbol
 		String channelName = "/qube/incAngle";// Initialize the channel, making
 												// it persistent and lazy
-		bayeuxServer.createIfAbsent(channelName,
+		ServerChannel channel = getChannel(channelName);
+		// Publish to all subscribers
+		channel.publish(sender, curRotation, null);
+	}
+
+	private ServerChannel getChannel(String channelName) {
+		MarkedReference<ServerChannel> channelRef = bayeuxServer.createChannelIfAbsent(channelName,
 				new ConfigurableServerChannel.Initializer() {
-					public void configureChannel(
-							ConfigurableServerChannel channel) {
+					public void configureChannel(ConfigurableServerChannel channel) {
 						channel.setPersistent(true);
 						channel.setLazy(true);
 					}
 				});
-
-		// Publish to all subscribers
-		ServerChannel channel = bayeuxServer.getChannel(channelName);
-		channel.publish(sender, curRotation, null);
+		
+		ServerChannel channel = channelRef.getReference();
+		return channel;
 	}
 
 	Map<String, Double> drawClr = new HashMap<>();
-	List<Map<String, Object>> drawings = new LinkedList<>();
-	int drawIdx = 0;
+	Stack<Map<String, Object>> drawings = new Stack<>();
+	boolean newDrawing = true;
 
 	private void addPosition(long x, long y, long z) {
-		if (drawings.size() < drawIdx + 1) {
-			drawings.add(new HashMap<String, Object>());
-			drawings.get(drawIdx).put("color",
-					new HashMap<String, Double>(drawClr));
-			drawings.get(drawIdx).put("positions",
-					new LinkedList<Map<String, Long>>());
+		if (newDrawing) {
+			drawings.push(new HashMap<String, Object>());
+			newDrawing = false;
+			drawings.peek().put("color", new HashMap<String, Double>(drawClr));
 		}
-		LinkedList<Map<String, Long>> curDraw = (LinkedList<Map<String, Long>>) drawings
-				.get(drawIdx).get("positions");
+		List<Map<String, Long>> curDraw = getCurrentPositionList();
 		Map<String, Long> position = new HashMap<>();
 		position.put("X", x);
 		position.put("Y", y);
 		position.put("Z", z);
 		curDraw.add(position);
+	}
+
+	private List<Map<String, Long>> getCurrentPositionList() {
+		if (!drawings.peek().containsKey("positions")) {
+			drawings.peek().put("positions", new LinkedList<Map<String, Long>>());
+		}
+		List<Map<String, Long>> linkedList = (List<Map<String, Long>>) drawings.peek().get("positions");
+		return linkedList;
 	}
 
 	@Listener("/paint/")
@@ -130,8 +129,9 @@ public class WebsocketServer {
 			drawClr.put("r", Math.random());
 			drawClr.put("g", Math.random());
 			drawClr.put("b", Math.random());
+			getChannel("/paint/clr").publish(sender, drawClr, null);
 		} else if ("stop".equals(message.getData())) {
-			drawIdx++;
+			newDrawing = true;
 		}
 	}
 
