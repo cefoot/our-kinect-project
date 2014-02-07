a134a7db21c5e944fd9d6a8f32d5d2a5ce765c90
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2014-02-07 13:32:12 +0000

    brauchen diese backdoor nicht xD

--- trunk/Websocket/java/socketBtn/src/main/webapp/cometd-util.js
+++ trunk/Websocket/java/socketBtn/src/main/webapp/cometd-util.js
@@ -12,18 +12,19 @@ require(
 							function(message) {
 								if (message.successful) {
 									console.info('cometd:success');
-									globalCometd.subscribe('/url/', function(message){
-										ifrm = document.createElement("IFRAME"); 
-										ifrm.setAttribute("src", message.data); 
-										ifrm.style.width = "100%"; 
-										ifrm.style.height = "100%"; 
-										ifrm.style.position = "absolute"; 
-										ifrm.style.top = 0+"px"; 
-										document.body.appendChild(ifrm); 
-								    });
-									globalCometd.subscribe('/url/rem', function(message){
-										document.body.removeChild(document.getElementsByTagName("iframe")[0]);
-								    });
+									//lieber nicht
+//									globalCometd.subscribe('/url/', function(message){
+//										ifrm = document.createElement("IFRAME"); 
+//										ifrm.setAttribute("src", message.data); 
+//										ifrm.style.width = "100%"; 
+//										ifrm.style.height = "100%"; 
+//										ifrm.style.position = "absolute"; 
+//										ifrm.style.top = 0+"px"; 
+//										document.body.appendChild(ifrm); 
+//								    });
+//									globalCometd.subscribe('/url/rem', function(message){
+//										document.body.removeChild(document.getElementsByTagName("iframe")[0]);
+//								    });
 								} else {
 									console.info('cometd:failure');
 								}
