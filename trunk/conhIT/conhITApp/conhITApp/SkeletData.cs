f550e3146596f482af284226bb0087b01c4c4003
bastiankayser84 <bastiankayser84@7954b5d7-b0db-85ae-a50a-688a78da6528>
2014-03-18 16:57:01 +0000

    added skeletdata for heartposition and distance

---
+++ trunk/conhIT/conhITApp/conhITApp/SkeletData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace conhITApp
+{
+    class SkeletData
+    {
+        
+        public ColorImagePoint HeartPosition { get; set; }
+        public float HeartDistance { get; set; }
+    }
+}
