bd6c57f1798b25e158e4cb2d0c4036e556e8a41c
bastiankayser84 <bastiankayser84@7954b5d7-b0db-85ae-a50a-688a78da6528>
2014-03-18 16:10:36 +0000

    added some kinect stuff and method for extracting position more or less cheese

--- conhIT/conhITApp/conhITApp/MainWindow.xaml.cs
+++ conhIT/conhITApp/conhITApp/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
 
 namespace conhITApp
 {
@@ -19,9 +20,69 @@ namespace conhITApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        KinectSensor kinect;
+        
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitKinectSensor();
+        }
+
+        private void InitKinectSensor()
+        {
+            this.kinect = KinectSensor.KinectSensors.Where(x => x.Status == KinectStatus.Connected).FirstOrDefault();
+            this.kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(Sensor_SkeletonFrameReady);
+            this.kinect.SkeletonStream.Enable();
+            this.kinect.Start();
+        }
+
+        void Sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                if (frame != null)
+                {
+
+                    Skeleton[] skeletons = new Skeleton[frame.SkeletonArrayLength];
+
+                    frame.CopySkeletonDataTo(skeletons);
+
+                    foreach (Skeleton skeleton in skeletons.Where(SkeletTracked))
+                    {
+                        if (skeleton != null)
+                        {
+
+                            if (IsTrackedOrInferred(skeleton.Joints[JointType.Spine]) && IsTrackedOrInferred(skeleton.Joints[JointType.ShoulderLeft]))
+                            {
+                                
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        private bool IsTrackedOrInferred(Joint joint)
+        {
+            return joint.TrackingState == JointTrackingState.Tracked || joint.TrackingState == JointTrackingState.Inferred;
+        }
+
+        
+
+        private bool SkeletTracked(Skeleton skelet)
+        {
+            var tracked = skelet.TrackingState == SkeletonTrackingState.Tracked;
+            return tracked;
+        }
+
+        
     }
 }
