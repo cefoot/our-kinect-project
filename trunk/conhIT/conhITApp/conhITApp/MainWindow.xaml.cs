34548ebac2d82fa7b60e418f74cdc19ed53ac01d
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2014-03-20 16:06:24 +0000

    smily faces hinzugefügt

--- trunk/conhIT/conhITApp/conhITApp/MainWindow.xaml.cs
+++ trunk/conhIT/conhITApp/conhITApp/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -15,6 +16,9 @@ using System.Windows.Shapes;
 using Microsoft.Kinect;
 using Coding4Fun.Kinect.Wpf;
 using WpfAnimatedGif;
+using Color = System.Drawing.Color;
+using Image = System.Windows.Controls.Image;
+using Size = System.Windows.Size;
 
 namespace conhITApp
 {
@@ -27,6 +31,7 @@ namespace conhITApp
         KinectSensor kinect;
         private readonly Dictionary<int,SkeletData> _skeletData = new Dictionary<int, SkeletData>();
         private Dictionary<int,Image> _imgs = new Dictionary<int, Image>();
+        private BitmapSource _face;
 
         public MainWindow()
         {
@@ -36,6 +41,9 @@ namespace conhITApp
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             InitKinectSensor();
+            var bitmap = Properties.Resources.face;
+            bitmap.MakeTransparent(Color.Blue);
+            _face = CreateBitmapSourceFromBitmap(bitmap);
         }
 
         private void InitKinectSensor()
@@ -57,9 +65,26 @@ namespace conhITApp
                 image.CopyPixelDataTo(data);
                 var myDrawingGroup = new DrawingGroup();
                 myDrawingGroup.Children.Add(new ImageDrawing(data.ToBitmapSource(image.Width, image.Height), new Rect(new Size(image.Width, image.Height))));
+
+                DrawFaces(myDrawingGroup);
+
                 image1.Source = new DrawingImage(myDrawingGroup);
             }
 
+        }
+
+        private void DrawFaces(DrawingGroup myDrawingGroup)
+        {
+            foreach (var skeletData in _skeletData.Values)
+            {
+                var calcWidth = 50d / (skeletData.HeadDistance / 3d);
+                var calcHeight = 52.5d / (skeletData.HeadDistance / 3d);
+
+                myDrawingGroup.Children.Add(new ImageDrawing(_face, new Rect(skeletData.HeadPosition.X - ((int)calcWidth / 2), skeletData.HeadPosition.Y - ((int)calcHeight / 2), calcWidth, calcHeight)));
+
+                tblHeight.Margin = new Thickness(skeletData.HeadPosition.X, skeletData.HeadPosition.Y, 0, 0);
+                //myDrawingGroup.Children.Add(new ImageDrawing(c. Properties.Resources.face, new Rect(40, 0, 45, 130)));
+            }
         }
 
         void Sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
@@ -82,6 +107,7 @@ namespace conhITApp
                             var spine = skeleton.Joints[JointType.Spine];
                             var shoulderLeft = skeleton.Joints[JointType.ShoulderLeft];
                             var shoulderRight = skeleton.Joints[JointType.ShoulderRight];
+                            var head = skeleton.Joints[JointType.Head];
                             if (IsTrackedOrInferred(spine) && IsTrackedOrInferred(shoulderLeft) && IsTrackedOrInferred(shoulderRight))
                             {
                                 var spineImagePos = kinect.CoordinateMapper.MapSkeletonPointToColorPoint(spine.Position, imageFormat);
@@ -95,6 +121,8 @@ namespace conhITApp
                                 skeletDataObject.HeartPosition = heartPos;
                                 skeletDataObject.HeartDistance = spine.Position.Z;
                                 skeletDataObject.heartWidth = Math.Abs(shoulderRightImagePos.X - shoulderLeftImagePos.X) * 0.6f;
+                                skeletDataObject.HeadPosition = kinect.CoordinateMapper.MapSkeletonPointToColorPoint(head.Position, imageFormat);
+                                skeletDataObject.HeadDistance = head.Position.Z;
                                 
                                 _skeletData[skeleton.TrackingId]=skeletDataObject;
 
@@ -157,12 +185,44 @@ namespace conhITApp
             }
         }
 
+        public static BitmapSource CreateBitmapSourceFromBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            using (var memoryStream = new MemoryStream())
+            {
+                try
+                {
+                    // You need to specify the image format to fill the stream. 
+                    // I'm assuming it is PNG
+                    bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    var bitmapDecoder = BitmapDecoder.Create(
+                        memoryStream,
+                        BitmapCreateOptions.PreservePixelFormat,
+                        BitmapCacheOption.OnLoad);
+
+                    // This will disconnect the stream from the image completely...
+                    var writable = new WriteableBitmap(bitmapDecoder.Frames.Single());
+                    writable.Freeze();
+
+                    return writable;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
         private Image CreateHeart()
         {
             var heartImg = new Image();
             var image = new BitmapImage();
             image.BeginInit();
-            image.UriSource = new Uri(@"pack://application:,,,/Resources/giphy.gif", UriKind.RelativeOrAbsolute);
+            image.UriSource = new Uri(@"pack://application:,,,/Resources/giphyBright.gif", UriKind.RelativeOrAbsolute);
             image.EndInit();
             ImageBehavior.SetAnimatedSource(heartImg, image);
             heartImg.VerticalAlignment = VerticalAlignment.Top;
