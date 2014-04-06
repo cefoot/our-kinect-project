841055b1a21e385d4a7329f7e26d9a012d79e66e
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2014-04-06 14:28:39 +0000

    Hackaton Projekt ... leider läuft das sphero sdk nicht unter win7.. daher unvollständig

---
+++ trunk/Sphernecto/Sphernecto/Extension.cs
@@ -0,0 +1,41 @@
+﻿/* $HeadURL$
+  ------------------------------------------------------------------------------
+        (c) by data experts gmbh
+              Postfach 1130
+              Woldegker Str. 12
+              17001 Neubrandenburg
+
+  Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+  dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+  Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+  noch manipuliert werden.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Sphernecto
+{
+
+    public static class Extension
+    {
+
+        public static BitmapSource ToBitmapSource(this byte[] pixels, int width, int height)
+        {
+            return ToBitmapSource(pixels, width, height, PixelFormats.Bgr32);
+        }
+
+        public static bool IsInRange(this float cur,float desired,float range)
+        {
+            return cur > desired - range && cur < desired + range;
+        }
+
+        private static BitmapSource ToBitmapSource(this byte[] pixels, int width, int height, PixelFormat format)
+        {
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, width * format.BitsPerPixel / 8);
+        }
+    }
+}
---
+++ trunk/Sphernecto/Sphero/Samples/Windows8/ConnectionSample/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Sphero.Communication;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ConnectionSample
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private async void DiscoverSpheros()
+        {
+            try
+            {
+                // Discover paired Spheros
+                List<SpheroInformation> spheroInformations = new List<SpheroInformation>(await SpheroConnectionProvider.DiscoverSpheros());
+
+                if (spheroInformations != null && spheroInformations.Count > 0)
+                {
+                    // Populate list with Discovered Spheros
+                    SpherosDiscovered.ItemsSource = spheroInformations;
+                }
+                else
+                {
+                    // No sphero Paired
+                    MessageDialog dialogNSP = new MessageDialog("No sphero Paired");
+                    await dialogNSP.ShowAsync();
+                }
+
+            }
+            catch (NoSpheroFoundException)
+            {
+                MessageDialog dialogNSF = new MessageDialog("No sphero Found");
+                dialogNSF.ShowAsync();
+            }
+            catch (BluetoothDeactivatedException)
+            {
+                // Bluetooth deactivated
+                MessageDialog dialogBD = new MessageDialog("Bluetooth deactivated");
+                dialogBD.ShowAsync();
+            }
+        }
+
+        private async void btnConnection_Click(object sender, RoutedEventArgs e)
+        {
+            if(SpherosDiscovered.SelectedItem != null)
+            {
+                SpheroConnection connection = await SpheroConnectionProvider.CreateConnection((SpheroInformation)SpherosDiscovered.SelectedItem);
+                Frame.Navigate(typeof(ConnectedPage), connection);
+            }
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            DiscoverSpheros();
+            base.OnNavigatedTo(e);
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            DiscoverSpheros();
+        }
+    }
+}
