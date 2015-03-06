a10f8381531d1b70fb8f55ad3827fe19127c6bb4
chriseric.pape@gmail.com <chriseric.pape@gmail.com@7954b5d7-b0db-85ae-a50a-688a78da6528>
2015-03-06 08:49:39 +0000

    Umstrukturierung

---
+++ trunk/conhIT/conhITApp/conhITApp-Old/SkeletData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace De.DataExperts.conhITApp
+{
+    class SkeletData
+    {
+        
+        public ColorSpacePoint HeartPosition { get; set; }
+        public float HeartDistance { get; set; }
+        public float heartWidth { get; set; }
+
+        public ColorSpacePoint HeadPosition { get; set; }
+
+        public double HeadDistance { get; set; }
+    }
+}
